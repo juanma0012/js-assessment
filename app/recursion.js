3671458e2b2c60841d560bb0ee31d29e4af5e216
Juan sanchez <juansanchez@AL-COL-02407.local>
2019-02-28 00:52:25 -0500

    Fixed all the methods for the recursion.js file

--- app/recursion.js
+++ app/recursion.js
@@ -9,27 +9,49 @@ recursionAnswers = {
       }
    *
    * Where ... is the same type of object
-   * 
+   *
    * @param {fileSystemObject} data - a file system object as described above
    * @param {String} dirName - a directory name the files are desired to be listed from.
    * Note: This parameter is optional. If it is not provided, list ALL files.
-   * 
+   *
    * @returns {Number[]} The files under the directory dirName, including subdiretories.
    */
-  listFiles: function listFiles(data, dirName) {
+  listFiles: function listFiles(data, dirName = '') {
+    if (dirName === '') {
+      dirName = data.dirName;
+    }
+    const findFiles = (directory, directoryName, countAll = false) => {
+      let foundFiles = [];
+      if (directoryName === directory.dirName || countAll) {
+        foundFiles = foundFiles.concat(directory.files);
+        countAll = true;
+      }
+      if (data.subDirs) {
+        let subDirIndex = 0;
+        while (subDirIndex < directory.subDirs.length) {
+          foundFiles = foundFiles.concat(findFiles(directory.subDirs[subDirIndex], directoryName, countAll));
+          subDirIndex++;
+        }
+      }
+      return foundFiles;
+    };
 
+    return findFiles(data, dirName);
   },
 
   /**
    * Determines the fibonacci number at position n.
    * https://en.wikipedia.org/wiki/Fibonacci_number
-   * 
+   *
    * The first few fibonacci numbers are: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
-   * 
+   *
    * @param {Number} n - the index of the fibonacci number desired
    * @returns {Number} The nth fibonacci number
    */
   fibonacci: function fibonacci(n) {
-
+    if (n < 2) {
+      return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
   },
 };
